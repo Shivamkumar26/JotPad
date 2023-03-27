016a6bc5c894854ea911de7cf17dfa332952f74c
Shivam kumar <99862619+Shivamkumar26@users.noreply.github.com>
2023-03-27 20:21:15 +0530

    Uploading files

---
+++ README.md
@@ -0,0 +1,60 @@
+# JotPad 📝
+
+I have created a notes website that offers a range of features to make note-taking easy and efficient.
+
+Checkout demo Live : - **[JotPad Demo](sillyy-cupcake.netlify.app)**
+
+## Features of JotPad
+
+The website allows users to -
+
+- Create new notes
+- Update existing notes
+- Delete unwanted notes
+- Restore Deleted notes
+- Archive Notes
+- Unarchive Notes
+- Search functionality that allows users to search through their saved notes.
+- Search in Home Page, Deleted Notes and Archived Notes
+
+
+## Learnings 🧠
+
+- **JavaScript event listeners:** I implemented JavaScript event listeners to make the buttons work.
+
+- **Local storage:** I learned how to use local storage to store and retrieve data from the browser. I used this to save all notes data i.e. notes title and description.
+
+- **Data manipulation:** I learned how to manipulate data using JavaScript. This included deleting, accessing and modifying data stored in local storage.
+
+- **Functions:** I used functions to organize the code. This is a key concept in JavaScript, and it helped me to make my code more modular and easier to read.
+
+- **Nesting:** I learned how to nest elements and functions in my HTML and JavaScript code. This was useful for organizing and structuring my code in a logical way.
+
+- **Boostrap:** I have used bootstrap and CSS for styling of the JotPad webapp.
+
+
+
+
+## Screenshots 📸 (To be updated)
+
+### Home Page
+![JotPad Homepage](https://user-images.githubusercontent.com/99862619/226272239-0a060402-2eb3-443b-b8a3-d9f9a2e3e8fa.png)
+
+### Add a new note
+![Add Note 1](https://user-images.githubusercontent.com/99862619/226272776-e02f3924-d375-4b7b-b955-30e19d7f8328.png)
+![Add Note 2](https://user-images.githubusercontent.com/99862619/226272798-28f1ea41-c0ce-45ec-9d46-d77a7e0967f6.png)
+
+### Update a note 
+![Updating snapshot 1](https://user-images.githubusercontent.com/99862619/226272947-1539e9cf-eab4-4781-bd6f-f54bdab4d670.png)
+![Updating snapshot 2](https://user-images.githubusercontent.com/99862619/226272954-86102be9-43e5-445d-a2d0-236ea67ef521.png)
+
+### Search a note
+![Search snapshot 1](https://user-images.githubusercontent.com/99862619/226273060-7e64312f-f049-424e-822e-62181e212172.png)
+![Search snapshot 2](https://user-images.githubusercontent.com/99862619/226273073-12dd339a-c58b-410c-8600-a11208ef6ca3.png)
+
+### Delete a note
+![JotPad Deleted a note](https://user-images.githubusercontent.com/99862619/226273182-f7baa4eb-4015-4d37-8e0d-940f26e5e145.png)
+
+## 🚀 Connect with me 
+
+Checkout my Github Profile: **[Shivam Kumar](https://github.com/Shivamkumar26/)**
---
+++ script.js
@@ -0,0 +1,241 @@
+let addNoteContainer = document.getElementById("addNoteContainer");
+
+// Show all the notes on HomePage
+function existingNotes() {
+    addNoteContainer.style.display = "none"; 
+    let allExistingNotes;
+    let notes = localStorage.getItem("notes");
+
+    if (notes === null) {
+        allExistingNotes = [];
+    } else {
+        allExistingNotes = JSON.parse(notes);
+    }
+
+    let notesContainer = document.getElementById("notes");
+    notesContainer.innerHTML = ""; 
+    allExistingNotes.forEach((note, index) => {
+        let latestNote = `
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">${note.title}</h5>
+                    <p class="card-text">${note.desc}</p>
+                    <button class="btn btn-warning card_btns" onClick="editNote(${index})"><i class="fas fa-solid fa-pen edit_btn"> </i></button>
+                    <button class="btn btn-warning card_btns" onClick="archiveNote(${index})"><i class="fas fa-solid fa-folder-plus"></i></button>
+                    <button class="btn btn-warning card_btns" onClick="deleteNote(${index})"><i class="fas fa-solid fa-trash delete_btn"></i></button>
+                </div>
+            </div>
+            `;
+        notesContainer.innerHTML = notesContainer.innerHTML + latestNote;
+    });
+}
+existingNotes();
+
+// Making "+ Add" button on navbar working
+let navAddNote = document.getElementById("navAddNote");
+navAddNote.addEventListener('click', ()=> {
+    let title = document.getElementById("title");
+    let desc = document.getElementById("desc");
+    title.value = "";
+    desc.value = "";
+    addNoteContainer.style.display = "block";
+})
+
+// Saving a note in local Storage  
+let addNote = document.getElementById("addNote");
+addNote.addEventListener('click', ()=> {
+    let allExistingNotes;
+    let notes = localStorage.getItem("notes");
+    if (notes === null) { 
+        allExistingNotes = [];
+    } else {
+        allExistingNotes = JSON.parse(notes); 
+    }
+
+    let title = document.getElementById("title");
+    let desc = document.getElementById("desc");
+    let newNoteObj = {
+        title : title.value,
+        desc : desc.value
+    }
+
+    if(existingNotes.innerText === "Update") {
+        let editCard = document.querySelector('.card');
+        let editIndex = editCard.getAttribute("editIndex");
+        allExistingNotes[editIndex] = newNoteObj;
+    }
+    else {
+        allExistingNotes.push(newNoteObj);
+    }
+    localStorage.setItem("notes",JSON.stringify(allExistingNotes))
+    title.value = "";
+    desc.value = "";    
+    existingNotes();
+})
+
+let cancelBtn = document.getElementById("cancelNote");
+cancelBtn.addEventListener('click', ()=> {
+    addNoteContainer.style.display = "none";
+})
+
+// Making our delete button work
+function deleteNote(noteIndex) {
+    let allExistingNotes = JSON.parse(localStorage.getItem("notes"));
+    let deletedNotes = JSON.parse(localStorage.getItem("deletedNotes"));
+    
+    if (deletedNotes === null) {
+        deletedNotes = [];
+    }
+    let deletedNote = allExistingNotes.splice(noteIndex, 1)[0];
+    deletedNotes.push(deletedNote);
+
+    localStorage.setItem("notes", JSON.stringify(allExistingNotes));
+    localStorage.setItem("deletedNotes", JSON.stringify(deletedNotes));
+    existingNotes();
+}
+
+// Making Delete Note on the Navbar Working 
+function deletedNotes() {
+    addNoteContainer.style.display = "none";
+    let allDeletedNotes;
+    let deletedNotes = localStorage.getItem("deletedNotes"); //Creating objects 
+
+    if (deletedNotes === null) {
+        allDeletedNotes = [];
+    } else {
+        allDeletedNotes = JSON.parse(deletedNotes);
+    }
+
+    //showing items on screen 
+    let notesContainer = document.getElementById("notes");
+    notesContainer.innerHTML = "";
+    allDeletedNotes.forEach((note, index) => {
+        let latestNote = `
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">${note.title}</h5>
+                    <p class="card-text">${note.desc}</p>
+                    <button class="btn btn-warning card_btns" onClick="undoDelete(${index})"><i class="fas fa-solid fa-undo undoDelete"></i></button>
+                </div>
+            </div>
+        `;
+        notesContainer.innerHTML = notesContainer.innerHTML + latestNote;
+    });
+}
+let navDeletedNotes = document.getElementById("navDeletedNotes");
+navDeletedNotes.addEventListener('click', ()=> {
+    deletedNotes();
+})
+
+// Making undoDelete button work
+function undoDelete(noteIndex) {
+    let deletedNotes = JSON.parse(localStorage.getItem("deletedNotes"));
+    let allExistingNotes = JSON.parse(localStorage.getItem("notes"));
+
+    if (allExistingNotes === null) {
+        allExistingNotes = [];
+    }
+    let allExistingNote = deletedNotes.splice(noteIndex, 1)[0];
+    allExistingNotes.push(allExistingNote);
+
+    localStorage.setItem("notes", JSON.stringify(allExistingNotes));
+    localStorage.setItem("deletedNotes", JSON.stringify(deletedNotes));
+    existingNotes();
+}
+
+// Making edit Note button work
+function editNote(noteIndex) {
+    let allExistingNotes = JSON.parse(localStorage.getItem("notes"));
+    addNoteContainer.style.display = 'block';
+    existingNotes.innerText = "Update"
+
+    let title = document.getElementById('title');
+    let desc = document.getElementById('desc');
+
+    title.value = allExistingNotes[noteIndex].title;
+    desc.value = allExistingNotes[noteIndex].desc;
+
+    let editCard = document.querySelector('.card');
+    editCard.setAttribute("editIndex", `${noteIndex}`);
+}
+
+// Search/Sort Functionality
+let search = document.getElementById("search");
+search.addEventListener('input', ()=> {
+    let inputSearch = search.value.toLowerCase();
+    let allCardSearch = document.getElementsByClassName("card");
+
+    Array.from(allCardSearch).forEach((ele) => {
+        let cardSearch = ele.getElementsByTagName('p')[0].innerText;
+
+        if(cardSearch.toLowerCase().includes(inputSearch)) {
+            ele.style.display = 'block';
+        } else {
+            ele.style.display = 'none';
+        }
+    })
+})
+
+// Button Archive Note on Navbar
+function archivedNote() {
+    addNoteContainer.style.display = "none";
+    let allArchivedNote;
+    let archivedNotes = localStorage.getItem("archivedNotes");
+
+    if (archivedNotes === null) {
+        allArchivedNote = [];
+    } else {
+        allArchivedNote = JSON.parse(archivedNotes); 
+    }
+
+    let notesContainer = document.getElementById("notes");
+    notesContainer.innerHTML = "";
+    allArchivedNote.forEach((note, index) => {
+        let latestNote = `
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">${note.title}</h5>
+                    <p class="card-text">${note.desc}</p>
+                    <button class="btn btn-warning card_btns" onClick="undoArchive(${index})"><i class="fas fa-solid fa-download undoArchive"></i></button>
+                </div>
+            </div>
+        `;
+        notesContainer.innerHTML = notesContainer.innerHTML + latestNote;
+    });
+}
+let navArchivedNotes = document.getElementById("navArchivedNotes");
+navArchivedNotes.addEventListener('click', ()=> {
+    archivedNote(); 
+})
+
+// Archive Note inside a note
+function archiveNote(noteIndex) {
+    let allExistingNotes = JSON.parse(localStorage.getItem("notes"));
+    let archivedNotes = JSON.parse(localStorage.getItem("archivedNotes"));
+    
+    if (archivedNotes === null) {
+        archivedNotes = [];
+    }
+    let archivedNote = allExistingNotes.splice(noteIndex, 1)[0];
+    archivedNotes.push(archivedNote);
+
+    localStorage.setItem("notes", JSON.stringify(allExistingNotes));
+    localStorage.setItem("archivedNotes", JSON.stringify(archivedNotes));
+    existingNotes();
+}
+
+// Unarchive a Note
+function undoArchive(noteIndex) {
+    let archivedNotes = JSON.parse(localStorage.getItem("archivedNotes"));
+    let allExistingNotes = JSON.parse(localStorage.getItem("notes"));
+
+    if (allExistingNotes === null) {
+        allExistingNotes = [];
+    }
+    let allExistingNote = archivedNotes.splice(noteIndex, 1)[0];
+    allExistingNotes.push(allExistingNote);
+
+    localStorage.setItem("notes", JSON.stringify(allExistingNotes));
+    localStorage.setItem("archivedNotes", JSON.stringify(archivedNotes));
+    existingNotes();
+}
